a922dd21a6988fce393ccc9c794e48a3857b96f5
madraks <rakib-123@hotmail.com>
2023-10-02 13:41:27 +0100

    task 3 to get a description of all apis, updated tests for task 2. implemented error handling for file not found

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const db = require("../db/connection.js");
 const request = require('supertest');
 const seed = require("../db/seeds/seed.js")
 const data = require("../db/data/test-data/index.js");
-
 beforeEach(() => {
   return seed(data);
 });
@@ -21,7 +20,8 @@ describe("GET /api/topics", () => {
   it('should return an array of topic objects with two properties: slug and description', () => {
     return request(app)
       .get("/api/topics")
-      .then(({body}) => {
+      .then(({ body }) => {
+        expect(body.topics).toHaveLength(3);
         body.topics.forEach((topic) => {
           expect(topic).toHaveProperty('slug', expect.any(String));
           expect(topic).toHaveProperty('description', expect.any(String));
@@ -32,9 +32,39 @@ describe("GET /api/topics", () => {
     return request(app)
       .get("/api/toooopics")
       .expect(404)
-      .then(({body}) => {
+      .then(({ body }) => {
         expect(body).not.toBe(undefined);
         expect(body.message).toBe('404: Path not found');
+      })
+  })
+})
+
+describe('GET /api', () => {
+  it('should return a status 200', () => {
+    return request(app)
+      .get('/api')
+      .expect(200)
+  })
+  it('should return an object that describes all available endpoints. each endpoint should have the properties description, queries, and exampleResponse', () => {
+    return request(app)
+      .get('/api')
+      .then(({ body }) => {
+        expect(Array.isArray(body)).not.toBe(true);
+        expect(typeof body).toBe("object");
+        for (const end in body.endpoints) {
+          const object = body.endpoints[end];
+          expect(object).toHaveProperty('description', expect.any(String));
+          expect(object).toHaveProperty('queries', expect.any(Array));
+          expect(object).toHaveProperty('exampleResponse', expect.any(Object));
+        }
+      })
+  })
+  it('should return with status 404 when path is invalid', () => {
+    return request(app)
+      .get('/ai')
+      .expect(404)
+      .then(({body}) => {
+        expect(body.message).toBe('404: Path not found')
       })
   })
 })
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const { getAllTopics } = require('./controllers/topics.controllers.js');
+const {getAllApis} = require('./controllers/api.controllers.js');
 
 app.use(express.json());
+
+app.get("/api", getAllApis);
 
 app.get("/api/topics", getAllTopics);
 
@@ -11,6 +14,9 @@ app.all("/*", (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
+  if(err.status && err.message) {
+    res.status(err.status).send({message: err.message})
+  }
   res.status(500).send({message: "500: Internal Server Error"})
 })
 
